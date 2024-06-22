cd76000612bd26c352a1c2ef5fa07148e68694cb
MeRpHiE <asiri.abayakoon@gmail.com>
2024-06-23 01:20:49 +0530

    Adding project skeleton.

---
+++ src/main/java/com/core/sms_demo/SmsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.core.sms_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsDemoApplication.class, args);
+	}
+
+}
